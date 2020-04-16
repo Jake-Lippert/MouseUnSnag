200d9f2a16ee3231b838a9ec22a72cae335b2081
Jake Lippert <Jake.Lippert+GitHub@outlook.com>
2020-04-15 21:15:33 -0400

    Refactoring MSLLHOOKSTRUCT, MouseEvents(MouseUnSnag), SnagScreen, and Main out into their own files

---
+++ MouseUnSnag/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace MouseUnSnag
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Make sure the MouseUnSnag.exe has only one instance running at a time.
+			if (new Mutex(true, "__MouseUnSnag_EXE__", out var createdNew) == null || !createdNew)
+			{
+				Debug.WriteLine("Already running!! Quitting this instance...");
+				return;
+			}
+			else
+			{
+				new MouseEvents().Run(args);
+			}
+		}
+	}
+}
---
+++ MouseUnSnag/Windows/MSLLHOOKSTRUCT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace MouseUnSnag.Windows
+{
+	[StructLayout(LayoutKind.Sequential)]
+	public struct MSLLHOOKSTRUCT
+	{
+		public Point pt;
+		public uint mouseData;
+		public uint flags;
+		public uint time;
+		public IntPtr dwExtraInfo;
+	}
+}
